0ead995f4bad9e964eba1f30a4ab96e2a103f708
Cooper Hedger <unyboman@gmail.com>
2023-04-18 07:41:43 -0700

    Update media.js

--- api/media.js
+++ api/media.js
@@ -3,13 +3,12 @@ const fetch = require("node-fetch");
 const BING_API_KEY = process.env.BING_API_KEY;
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 const GOOGLE_CSE_ID = process.env.GOOGLE_CSE_ID;
-
 const BING_VIDEO_SEARCH_URL = `https://api.bing.microsoft.com/v7.0/custom/videos/search`;
 const GOOGLE_IMAGE_SEARCH_URL = `https://www.googleapis.com/customsearch/v1`;
 
 module.exports = async (req, res) => {
   const searchQuery = req.query.search;
-  const searchType = req.query.type;
+  const searchType = req.query.type || "image";
 
   if (!searchQuery) {
     res.status(400).json({ error: "Search query is required" });
@@ -24,45 +23,37 @@ module.exports = async (req, res) => {
     searchTypeParams = `&customconfig=474bf85a-27fb-4ca2-b874-baefb5cdcbcc&SafeSearch=Off&videoLength=Short&videoLicense=All&count=${resultCount}`;
   } else {
     searchUrl = GOOGLE_IMAGE_SEARCH_URL;
-    searchTypeParams = `&cx=${GOOGLE_CSE_ID}&searchType=image&num=${resultCount}&key=${GOOGLE_API_KEY}`;
+    searchTypeParams = `&cx=${GOOGLE_CSE_ID}&searchType=image&key=${GOOGLE_API_KEY}&num=${resultCount}`;
   }
 
   console.log(`Request URL: ${searchUrl}?q=${encodeURIComponent(searchQuery)}${searchTypeParams}`);
   const response = await fetch(
     `${searchUrl}?q=${encodeURIComponent(searchQuery)}${searchTypeParams}`,
     {
-      headers: searchType === "video"
-        ? {
-            "Ocp-Apim-Subscription-Key": BING_API_KEY,
-            "X-MSEdge-ClientID": "mediafetch",
-            "X-MSEdge-ClientIP": "0.0.0.0",
-          }
-        : {}
+      headers: {
+        "Ocp-Apim-Subscription-Key": BING_API_KEY,
+        "X-MSEdge-ClientID": "mediafetch",
+        "X-MSEdge-ClientIP": "0.0.0.0",
+      },
     }
   );
   const data = await response.json();
   console.log(`Response data:`, data);
 
-  if ((searchType === "video" && data.value && data.value.length > 0) || (searchType === "image" && data.items && data.items.length > 0)) {
-    const items = searchType === "video" ? data.value : data.items;
-    const randomIndex = Math.floor(Math.random() * items.length);
-    const item = items[randomIndex];
+  if (data.value && data.value.length > 0) {
+    const randomIndex = Math.floor(Math.random() * data.value.length);
+    const item = data.value[randomIndex];
     const mediaUrl = searchType === "video" ? item.contentUrl : item.link;
     console.log(`Original media URL: ${mediaUrl}`);
-    const mediaType = searchType === "video" ? "video/mp4" : "image/jpeg"; // You may need to adjust this based on the actual image format.
+    const mediaType = "image/jpeg"; // You may need to adjust this based on the actual image format.
 
-    if (searchType === "video") {
-      res.setHeader("X-Media-Type", mediaType);
-      res.json({ mediaUrl });
-    } else {
-      const mediaResponse = await fetch(mediaUrl);
-      const contentType = mediaResponse.headers.get("content-type");
-      const mediaData = await mediaResponse.buffer();
+    const mediaResponse = await fetch(mediaUrl);
+    const contentType = mediaResponse.headers.get("content-type");
+    const mediaData = await mediaResponse.buffer();
 
-      res.setHeader("Content-Type", contentType);
-      res.setHeader("X-Media-Type", mediaType);
-      res.send(mediaData);
-    }
+    res.setHeader("Content-Type", contentType);
+    res.setHeader("X-Media-Type", mediaType);
+    res.send(mediaData);
   } else {
     res.status(404).json({ error: "No media found" });
   }
