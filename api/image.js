b5d40c0a5e33036b0a8ba3429f452fa1b07c47a5
Cooper Hedger <unyboman@gmail.com>
2023-04-18 01:17:08 -0700

    Create image.js

---
+++ api/image.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+
+const GOOGLE_API_KEY = "AIzaSyAUon6c2xVmcYvsSFkU064tq77QzuLmm64";
+const GOOGLE_CX = "166d93545867f4dbd";
+const GOOGLE_SEARCH_URL = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CX}&searchType=image`;
+
+module.exports = async (req, res) => {
+    const searchQuery = req.query.search;
+
+    if (!searchQuery) {
+        res.status(400).json({ error: "Search query is required" });
+        return;
+    }
+
+    const response = await fetch(`${GOOGLE_SEARCH_URL}&q=${encodeURIComponent(searchQuery)}`);
+    const data = await response.json();
+
+    if (data.items && data.items.length > 0) {
+        res.json({ imageUrl: data.items[0].link });
+    } else {
+        res.status(404).json({ error: "No images found" });
+    }
+};
