4fa6f7343a76ea30fc98b9aa4d0e98df6ff58e9a
Cooper Hedger <unyboman@gmail.com>
2023-04-18 02:40:05 -0700

    Update image.js

--- api/image.js
+++ api/image.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 const GOOGLE_API_KEY = "AIzaSyAUon6c2xVmcYvsSFkU064tq77QzuLmm64";
 const GOOGLE_CX = "166d93545867f4dbd";
-const GOOGLE_SEARCH_URL = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CX}&searchType=image`;
+const GOOGLE_SEARCH_URL = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CX}&searchType=video`;
 
 module.exports = async (req, res) => {
     const searchQuery = req.query.search;
